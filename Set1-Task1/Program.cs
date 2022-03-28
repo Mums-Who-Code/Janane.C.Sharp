9867558d41a1da008ebb79e3952a0b2bfb62c88d
Janane <imjanane@gmail.com>
2022-03-28 15:42:41 -0500

    CODE RUB : Set1Task2 store and retrieve literal variables values.

---
+++ Set1-Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Set1_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
