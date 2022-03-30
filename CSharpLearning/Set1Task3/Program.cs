2f160a7e37beff753792e5bd4081f3ac1d59580f
Janane <imjanane@gmail.com>
2022-03-29 23:01:32 -0500

    CODE RUB: Perform basic string formatting -set1task3

---
+++ CSharpLearning/Set1Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Set1Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Janane";
+            DateTime joiningDate = new DateTime(2021,08,23);
+
+            Console.WriteLine("My name is " +name+ ".");
+            Console.WriteLine("I joined MumsWhoCode on " +joiningDate+ ".");
+
+        }
+    }
+}
