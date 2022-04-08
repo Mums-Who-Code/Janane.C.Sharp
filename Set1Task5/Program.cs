e9badf0ef9c483db02179724df1f6a4560c8809e
Janane <imjanane@gmail.com>
2022-04-08 12:41:05 -0500

    CODE RUB: Call methods from the .NET Class Library(Set1Task5)

---
+++ Set1Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Set1Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            Random random = new Random();
+            int number = random.Next(1, 5);
+            Console.WriteLine("Random number: " +number);
+            today = today.AddDays(number);
+            Console.WriteLine("Date: " +today);          
+        }
+    }
+}
